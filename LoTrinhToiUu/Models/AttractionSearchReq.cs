cd2e1c9796ec5290d0513fdc1961dca38193a9d3
Zonat-2004 <1150080038@sv.hcmunre.edu.vn>
2025-08-30 14:21:26 +0700

    da push

---
+++ LoTrinhToiUu/Models/AttractionSearchReq.cs
@@ -0,0 +1,9 @@
+﻿namespace LoTrinhToiUu.Models
+{
+    public class AttractionSearchReq
+    {
+        public double[] Center { get; set; } = new double[2]; // [lng, lat]
+        public double RadiusKm { get; set; } = 5;             // Bán kính tìm kiếm
+        public string Keyword { get; set; } = "tourism";     // Loại POI
+    }
+}
